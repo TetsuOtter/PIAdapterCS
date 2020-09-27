09530699bc7e0a11076797d46161a3f824f3e886
ＴＲ <public@technotter.com>
2020-09-28 04:33:34 +0900

    Create ConstValues.cs
    定数値記録用クラスを追加

---
+++ PIAdapterCS/ConstValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIAdapterCS
+{
+	static public class ConstValues
+	{
+		static public uint ATSPI_IF_GetPIVersion { get; } = 0x00020000;
+	}
+}
