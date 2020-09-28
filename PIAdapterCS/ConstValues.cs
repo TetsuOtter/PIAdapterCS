341fa0f9f2975fdd14bb865ed5e8d2d6393e769b
ＴＲ <public@technotter.com>
2020-09-28 18:24:45 +0900

    PISyncerを追加
    異なるTargetPlatformなPI間で同期を図るためのクラスを作成.

--- PIAdapterCS/ConstValues.cs
+++ PIAdapterCS/ConstValues.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PIAdapterCS
+﻿namespace PIAdapterCS
 {
 	static public class ConstValues
 	{
 		static public uint ATSPI_IF_GetPIVersion { get; } = 0x00020000;
+
+		static public int PanelArrSize { get; } = 256;
+		static public int SoundArrSize { get; } = 256;
+
+
+		static public int PISyncer_Tick { get; } = 10;
+		static public int PISyncer_TimeoutCount { get; } = 1000;
+		static public int PISyncer_MMFFileBufSize { get; } = (sizeof(int) * 514) + 512;
+		static public string PISyncer_Name_Flags { get; } = "FLG";
+		static public string PISyncer_Name_PanelD { get; } = "PNL";
+		static public string PISyncer_Name_SoundD { get; } = "SND";
+		static public string PISyncer_Name_ArgData { get; } = "ARG";
+		static public string PISyncer_Name_Handle { get; } = "HND";
 	}
 }
---
+++ PIAdapterCS/PISyncer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace PIAdapterCS
+{
+	public class PISyncer : IDisposable
+	{
+		MemoryMappedFile MMF_Flags, MMF_PanelD, MMF_SoundD, MMF_ArgData, MMF_Handle;
+		private bool disposedValue;
+
+		public PISyncer(in string MMF_FileName)
+		{
+			if (!File.Exists(MMF_FileName))
+				File.Create(MMF_FileName, ConstValues.PISyncer_MMFFileBufSize);
+
+			MMF_Flags = MemoryMappedFile.CreateFromFile(MMF_FileName, FileMode.Open, ConstValues.PISyncer_Name_Flags);
+			MMF_PanelD = MemoryMappedFile.CreateFromFile(MMF_FileName, FileMode.Open, ConstValues.PISyncer_Name_PanelD);
+			MMF_SoundD = MemoryMappedFile.CreateFromFile(MMF_FileName, FileMode.Open, ConstValues.PISyncer_Name_SoundD);
+			MMF_ArgData = MemoryMappedFile.CreateFromFile(MMF_FileName, FileMode.Open, ConstValues.PISyncer_Name_ArgData);
+			MMF_Handle = MemoryMappedFile.CreateFromFile(MMF_FileName, FileMode.Open, ConstValues.PISyncer_Name_Handle);
+
+		}
+
+		public void SetSyncerFlag(in int index, in uint value)
+		{
+			using (var va = MMF_Flags.CreateViewAccessor(index, sizeof(uint)))
+				va.Write(0, va.ReadUInt32(0) | value);
+		}
+
+		public bool IsSyncerFlagRaised(in int index, in uint value)
+		{
+			using (var va = MMF_Flags.CreateViewAccessor(index, sizeof(uint)))
+				return (va.ReadUInt32(0) & value) == value;//AND演算の結果がCheckFlagsと一致するなら, CheckFlagsはすべて立ってる.
+		}
+
+
+		public void DoElapseFunc(Func<State,IntPtr,IntPtr,Hand> elapse, in int pnlarr_len, in int sndarr_len)
+		{
+			using (var va_pnl = MMF_PanelD.CreateViewAccessor(sizeof(int), sizeof(int) * pnlarr_len))
+			using (var va_snd = MMF_SoundD.CreateViewAccessor(sizeof(int), sizeof(int) * sndarr_len))
+			using (var va_hnd = MMF_Handle.CreateViewAccessor())
+			{
+				Hand h = elapse.Invoke(TouchArgData().state, va_pnl.SafeMemoryMappedViewHandle.DangerousGetHandle(), va_snd.SafeMemoryMappedViewHandle.DangerousGetHandle());
+				va_hnd.Write(0, ref h);
+			}
+		}
+
+		/// <summary>MemoryMappedFileにあるPanelデータを取得します</summary>
+		/// <param name="arr">書き込み先配列</param>
+		/// <param name="length">読み込む要素数</param>
+		public void GetPanelFromMMF(int[] arr, in int length)
+		{
+			using (var va = MMF_PanelD.CreateViewAccessor())
+				va.ReadArray(sizeof(int), arr, 0, length);//オフセットは将来の「可変配列長」機能用
+		}
+		
+		/// <summary>MemoryMappedFileのPanelデータを指定値で更新します</summary>
+		/// <param name="arr">書き込む値が記録された配列</param>
+		/// <param name="length">書き込む要素数</param>
+		public void SetPanelToMMF(in int[] arr, in int length)
+		{
+			using (var va = MMF_PanelD.CreateViewAccessor())
+				va.WriteArray(sizeof(int), arr, 0, length);//オフセットは将来の「可変配列長」機能用
+		}
+		
+		/// <summary>MemoryMappedFileにあるSoundデータを取得します</summary>
+		/// <param name="arr">書き込み先配列</param>
+		/// <param name="length">読み込む要素数</param>
+		public void GetSoundFromMMF(int[] arr, in int length)
+		{
+			using (var va = MMF_SoundD.CreateViewAccessor())
+				va.ReadArray(sizeof(int), arr, 0, length);//オフセットは将来の「可変配列長」機能用
+		}
+
+		/// <summary>MemoryMappedFileのSoundデータを指定値で更新します</summary>
+		/// <param name="arr">書き込む値が記録された配列</param>
+		/// <param name="length">書き込む要素数</param>
+		public void SetSoundToMMF(in int[] arr, in int length)
+		{
+			using (var va = MMF_SoundD.CreateViewAccessor())
+				va.WriteArray(sizeof(int), arr, 0, length);//オフセットは将来の「可変配列長」機能用
+		}
+
+		public ArgData TouchArgData(Action<ArgData> act = null)
+		{
+			ArgData ad;
+			using (var va_stt = MMF_ArgData.CreateViewAccessor())
+			{
+				va_stt.Read(0, out ad);
+				if (act != null)
+				{
+					act.Invoke(ad);//適当な操作
+					va_stt.Write(0, ref ad);//操作後の値を書き込み
+				}
+			}
+			return ad;
+		}
+
+		public Hand GetHandle()
+		{
+			Hand h;
+			using (var va = MMF_Handle.CreateViewAccessor())
+				va.Read(0, out h);
+
+			return h;
+		}
+
+		#region Dispose
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					MMF_Flags?.Dispose();
+					MMF_PanelD?.Dispose();
+					MMF_SoundD?.Dispose();
+					MMF_ArgData?.Dispose();
+					MMF_Handle?.Dispose();
+				}
+
+				MMF_Flags = MMF_PanelD = MMF_SoundD = MMF_ArgData = MMF_Handle = null;
+
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+		// ~PISyncer()
+		// {
+		//     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+
+	public struct ArgData
+	{
+		public State state;
+		public Spec spec;
+		public Beacon beacon;
+		public int intValue;
+	}
+
+	[Flags]
+	public enum SyncerFlags
+	{
+		Load = 1 << 0,
+		Dispose = 1 << 1,
+		GetPluginVersion = 1 << 2,
+		SetVehicleSpec = 1 << 3,
+		Initialize = 1 << 4,
+		Elapse = 1 << 5,
+		SetPower = 1 << 6,
+		SetBrake = 1 << 7,
+		SetReverser = 1 << 8,
+		DoorOpen = 1 << 9,
+		DoorClose = 1 << 10,
+		KeyDown = 1 << 11,
+		KeyUp = 1 << 12,
+		HornBlow = 1 << 13,
+		SetSignal = 1 << 14,
+		SetBeaconData = 1 << 15,
+	}
+}
