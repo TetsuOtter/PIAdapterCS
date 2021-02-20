7a5ddbccbbda18dbc113cc0a224622e10dbe4154
ＴＲ <public@technotter.com>
2021-02-20 21:35:35 +0900

    Create SameTargetATSPILoader.cs
    CLRなファイルもこちらで読み込むように変更

---
+++ SameTargetAtsPILoader/SameTargetATSPILoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace TR
+{
+	/// <summary>TargetPlatformが同じATSプラグインを操作します.</summary>
+	public static class SameTargetATSPILoader
+	{
+		static public IAtsPI LoadNativeOrClrPI(in string PIPath) => CreateInstanceFromILDll(PIPath) ?? new SameTargetNativeATSPI(PIPath);
+		static public IAtsPI? CreateInstanceFromILDll(in string path)
+		{
+			try
+			{
+				Assembly asm = Assembly.LoadFrom(path);
+				foreach (var m in asm.GetModules())
+				{
+					try
+					{
+						foreach (var t in m.GetTypes())
+							if (typeof(IAtsPI).IsAssignableFrom(t)) //IsAssignableFrom ref : https://smdn.jp/programming/netfx/tips/check_type_isassignablefrom_issubclassof/#section.2.1
+								return asm.CreateInstance(t.Name) as IAtsPI;
+					}
+					catch (Exception ex)
+					{
+						System.Diagnostics.Debug.WriteLine(ex);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(ex);
+			}
+
+			return null;
+		}
+
+	}
+}
