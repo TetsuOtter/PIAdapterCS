8cf3431883c5f7092dfeee1115cd51a8236582d9
ＴＲ <public@technotter.com>
2021-03-04 05:55:37 +0900

    CLR用のdllを読んだとき, 無駄に依存関係を読まずに型チェックを行うように修正

--- SameTargetAtsPILoader/SameTargetATSPILoader.cs
+++ SameTargetAtsPILoader/SameTargetATSPILoader.cs
@@ -11,14 +11,23 @@ namespace TR
 		{
 			try
 			{
-				Assembly asm = Assembly.LoadFrom(path);
+				//ReflectionOnlyLoadFrom ref : https://docs.microsoft.com/ja-jp/dotnet/framework/reflection-and-codedom/how-to-load-assemblies-into-the-reflection-only-context
+				//AppDomain ref : https://devlights.hatenablog.com/entry/20120808/p1
+				Assembly asm = Assembly.ReflectionOnlyLoadFrom(path);
 				foreach (var m in asm.GetModules())
 				{
 					try
 					{
 						foreach (var t in m.GetTypes())
+						{
+							System.Diagnostics.Debug.WriteLine(t);
 							if (typeof(IAtsPI).IsAssignableFrom(t)) //IsAssignableFrom ref : https://smdn.jp/programming/netfx/tips/check_type_isassignablefrom_issubclassof/#section.2.1
-								return asm.CreateInstance(t.Name) as IAtsPI;
+							{
+								System.Diagnostics.Debug.WriteLine($"IAtsPI : {t} (fullname:{asm.FullName}, typename:{t.FullName})");
+								if (asm.FullName is string asm_fn && t.FullName is string t_fn)//NULL Check
+									return new SameTargetIAtsPIPlugin(asm_fn, t_fn);
+							}
+						}
 					}
 					catch (Exception ex)
 					{
